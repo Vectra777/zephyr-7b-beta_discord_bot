04509a0f2b97b473befc7afffef7271dc104eb17
crafiouse <crafiouse@gmail.com>
2023-11-22 16:38:58 +0100

    return speech

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,22 @@
+import json
+import os
 import torch
 from transformers import pipeline
 
 pipe = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta", torch_dtype=torch.bfloat16, device_map="auto")
 
+
 while True:
-    a = input("User: ")
-    messages = [
-    {
-        "role": "system",
-        "content": "You are a pirate talking like a pirate each time",
-    },
-    {"role": "user", "content": a},
-]
-    prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-    print(outputs[0]["generated_text"])
+    if os.path.isfile("data.json"):
+        
+        messages = json.load(open("data.json", "r", encoding="utf-8"))
+        message = [
+            messages[0],
+            messages[1]
+        ]
+        prompt = pipe.tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
+        outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+        f= open("../output.txt", "w", encoding="utf-8")
+        f.write(outputs[0]["generated_text"])
+        f.close()
+        os.remove("data.json")
--- commands/chat/chat_bot.js
+++ commands/chat/chat_bot.js
@@ -3,16 +3,16 @@ const fs = require("node:fs")
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('chat')
-        .addStringOption(option =>
-			option
-				.setName('message'))
-		.setDescription('Ask the bot a question'),
+	.setName('message')
+	.setDescription('Replies with your input!')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('The input message')
+			.setRequired(true)),
 	async execute(interaction) {
 
-        const message = interaction.option.getString("message");
-
-        const data_send = [
+        const message = interaction.options.getString('input');
+		const data_send = [
             {
                 "role": "system",
                 "content": "You are a pirate talking like a pirate each time",
@@ -21,12 +21,20 @@ module.exports = {
         ];
         const data = JSON.stringify(data_send)
 
-        fs.writeFile('data.json', data, function (err) {
+        fs.writeFile('./bot/data.json', data, function (err) {
             if (err) throw err;
             console.log('Saved!');
           });
-          
-		return interaction.reply('Pong!');
+		  async function readFile(filePath) {
+			try {
+			  const data = await fs.readFile(filePath);
+			  return interaction.reply(data.toString());
+			} catch (error) {
+			  console.error(`Got an error trying to read the file: ${error.message}`);
+			}
+		  }
+
+		  return await readFile("../../data.txt")
 	},
 
 };
