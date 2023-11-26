fa7546b668bcc785994f81c3a867ad4f6a691d00
crafiouse <crafiouse@gmail.com>
2023-11-26 19:46:39 +0100

    fix

--- commands/chat/chat_bot.js
+++ commands/chat/chat_bot.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const fs = require("fs");
+const fs = require("fs").promises;
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,7 +10,6 @@ module.exports = {
                 .setDescription('The input message')
                 .setRequired(true)),
     async execute(interaction) {
-
         const message = interaction.options.getString('input');
         const data_send = [
             {
@@ -21,27 +20,40 @@ module.exports = {
         ];
         const data = JSON.stringify(data_send);
 
-        fs.writeFile('./bot/data.json', data, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
+        try {
+            // Write the data to the file
+            await fs.writeFile('./bot/data.json', data);
+            console.log('File saved!');
 
-        async function readFile(filePath) {
-            try {
-                let data = await fs.promises.readFile(filePath, "utf-8").then((data)=> (
-                    data.slice(data.indexOf("<|assistant|>")+13)
-                ));
-                await fs.promises.unlink(filePath)
-                return data;
-            } catch (error) {
-                console.error(`Got an error trying to read the file: ${error.message}`);
+            // Wait until the file is created
+            const outputFile = 'C:\\Users\\crafi\\Desktop\\discord_bot\\output.txt';
+            while (!(await fileExists(outputFile))) {
+                // Sleep for a short duration before checking again
+                await sleep(1000);
             }
+
+            // File is created, proceed with reading and responding
+            let output = await fs.readFile(outputFile, 'utf-8');
+            output = output.slice(output.indexOf("") + 13);
+
+            // Respond to the interaction
+            return interaction.reply(output.toString());
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            return interaction.reply('An error occurred while processing your command.');
         }
-
-        // Adjust the path to the correct location
-        let output = await readFile('output.txt');
-
-        // Respond to the interaction
-        interaction.reply(output.toString());
     },
 };
+
+async function fileExists(filePath) {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
